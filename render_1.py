601b071b1ae260aa556e5e0a80e99e9bd477b2e1
Mark Saiget <saiget@gmail.com>
2015-03-23 20:57:27 -0700

    Add zip functionality.

--- render_1.py
+++ render_1.py
@@ -6,6 +6,7 @@ import os
 import boto
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+import zipfile
 
 
 # lat = 38.9004204
@@ -25,11 +26,11 @@ sceneID = ['LC80030172015001LGN00']
 bands = [4, 3, 2]
 
 path = '/Users/mark/projects/landsat_worker/dl2'
-# sceneID='LC80030172015001LGN00'
-b = Downloader(verbose=True, download_dir=path)
-b.download(sceneID, bands)
+# # sceneID='LC80030172015001LGN00'
+# b = Downloader(verbose=True, download_dir=path)
+# b.download(sceneID, bands)
 input_path = os.path.join(path, sceneID[0])
-dest_path = input_path
+# dest_path = input_path
 
 # c = Process(input_path, bands=bands, dst_path=path, verbose=True)
 # c.run(pansharpen=False)
@@ -40,19 +41,23 @@ for i in bands:
     band_output = '{}{}'.format(band_output, i)
 file_name = '{}_bands_{}.TIF'.format(sceneID[0], band_output)
 file_location = os.path.join(input_path, file_name)
+file_zip = '{}_bands_{}.zip'.format(sceneID[0], band_output)
+zf = zipfile.ZipFile(file_zip, 'w', zipfile.ZIP_DEFLATED)
+zf.write(file_location)
+zf.close()
 
-file_name = 'test.png'
-file_location = '/Users/mark/Desktop/test.png'
-conne = boto.connect_s3()
-b = conne.get_bucket('landsatproject')
-k = Key(b)
-k.key = 'test'
-k.set_contents_from_filename(file_location)
-k.get_contents_to_filename(file_location)
-hello = b.get_key('test')
-# set to be public
-hello.set_canned_acl('public-read')
-hello.generate_url(0, query_auth=False, force_http=True)
+# file_name = 'test.png'
+# file_location = '/Users/mark/Desktop/test.png'
+# conne = boto.connect_s3()
+# b = conne.get_bucket('landsatproject')
+# k = Key(b)
+# k.key = 'test'
+# k.set_contents_from_filename(file_location)
+# k.get_contents_to_filename(file_location)
+# hello = b.get_key('test')
+# # set to be public
+# hello.set_canned_acl('public-read')
+# hello.generate_url(0, query_auth=False, force_http=True)
 
 
 # generates url that works for 1 hour
