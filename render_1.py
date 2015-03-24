21b797eff364298a22be34f40d073a9c0b628000
Mark Saiget <saiget@gmail.com>
2015-03-23 21:45:54 -0700

    Comment code. Pip freeze requirements.

--- render_1.py
+++ render_1.py
@@ -9,31 +9,18 @@ from boto.s3.key import Key
 import zipfile
 
 
-# lat = 38.9004204
-# lon = -77.0237117
-# a = Search()
-# output = a.search(lat=lat, lon=lon, limit=10)
-# print 'output length: {}'.format(len(output))
-# import pdb; pdb.set_trace()
-# # sceneID = output['results'][0]['sceneID']
-# # sceneID = []
-# # for i in output['results']:
-# #     sceneID.append(str(i['sceneID']))
-# sceneID = [str(i['sceneID']) for i in output['results']]
-# print sceneID
-
 sceneID = ['LC80030172015001LGN00']
 bands = [4, 3, 2]
 
 path = '/Users/mark/projects/landsat_worker/dl2'
-# # sceneID='LC80030172015001LGN00'
-# b = Downloader(verbose=True, download_dir=path)
-# b.download(sceneID, bands)
+# sceneID='LC80030172015001LGN00'
+b = Downloader(verbose=True, download_dir=path)
+b.download(sceneID, bands)
 input_path = os.path.join(path, sceneID[0])
-# dest_path = input_path
+dest_path = input_path
 
-# c = Process(input_path, bands=bands, dst_path=path, verbose=True)
-# c.run(pansharpen=False)
+c = Process(input_path, bands=bands, dst_path=path, verbose=True)
+c.run(pansharpen=False)
 
 band_output = ''
 
@@ -41,23 +28,25 @@ for i in bands:
     band_output = '{}{}'.format(band_output, i)
 file_name = '{}_bands_{}.TIF'.format(sceneID[0], band_output)
 file_location = os.path.join(input_path, file_name)
-file_zip = '{}_bands_{}.zip'.format(sceneID[0], band_output)
-zf = zipfile.ZipFile(file_zip, 'w', zipfile.ZIP_DEFLATED)
+
+# zip file, maintain location
+file_name_zip = '{}_bands_{}.zip'.format(sceneID[0], band_output)
+zf = zipfile.ZipFile(file_name_zip, 'w', zipfile.ZIP_DEFLATED)
 zf.write(file_location)
 zf.close()
 
-# file_name = 'test.png'
-# file_location = '/Users/mark/Desktop/test.png'
-# conne = boto.connect_s3()
-# b = conne.get_bucket('landsatproject')
-# k = Key(b)
-# k.key = 'test'
-# k.set_contents_from_filename(file_location)
-# k.get_contents_to_filename(file_location)
-# hello = b.get_key('test')
-# # set to be public
-# hello.set_canned_acl('public-read')
-# hello.generate_url(0, query_auth=False, force_http=True)
+# upload to s3
+file_location = os.path.join(input_path, file_name_zip)
+conne = boto.connect_s3()
+b = conne.get_bucket('landsatproject')
+k = Key(b)
+k.key = file_name_zip
+k.set_contents_from_filename(file_location)
+k.get_contents_to_filename(file_location)
+hello = b.get_key('test')
+# make public
+hello.set_canned_acl('public-read')
+hello.generate_url(0, query_auth=False, force_http=True)
 
 
 # generates url that works for 1 hour
