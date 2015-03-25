959b0d1da492cc7eb92ca47e3318edf363e176d2
Mark Saiget <saiget@gmail.com>
2015-03-25 13:51:54 -0700

    Update test.py to most recent send_request method.

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import requests
 
 
-def send_post_request(pic_url='http://test.com', pk='15'):
+
+def send_post_request(job_id, status=10, pic_url=None):
     """Send post request to pyramid app, to notify completion."""
-    payload = {'url': pic_url, 'pk': pk}
+    payload = {'url': pic_url, 'job_id': job_id, 'status': status}
     post_url = "http://localhost:8000/done"
-    r = requests.post(post_url, data=payload)
-    print "post request sent"
+    requests.post(post_url, data=payload)
+    # print "post request sent to {}".format(post_url)
+    if status == 5:
+        print 'job_id: {} done.'.format(job_id)
     return True
 
 if __name__ == '__main__':
-    send_post_request()
+    send_post_request(3, 5)
