e81ee8dc8ea342aef678d61469560a4b470724de
Mark Saiget <saiget@gmail.com>
2015-04-01 09:28:12 -0700

    Make small changes to render_1.py. Write render_little for generating previews from queue.

--- render_little.py
+++ render_little.py
@@ -1,3 +1,5 @@
+"""Render preview from queue and upload to s3 bucket."""
+
 import sys
 sys.path.append('landsat-util/landsat')
 from downloader import Downloader
@@ -5,12 +7,12 @@ from image import Process
 import os
 import boto
 from boto.s3.key import Key
-import zipfile
 import requests
 from sqs import (make_SQS_connection, get_queue, get_message, get_attributes,
                  delete_message_from_handle,)
 from shutil import rmtree
 import datetime
+import subprocess
 
 os.getcwd()
 path_download = os.getcwd() + '/download'
@@ -127,14 +129,34 @@ def checking_for_jobs():
 def process(job):
     '''Given bands and sceneID, download, image process, zip & upload to S3.'''
 
-    send_post_request(job['job_id'], 1)
     b = Downloader(verbose=True, download_dir=path_download)
     scene_id = [str(job['scene_id'])]
     bands = [job['band_1'], job['band_2'], job['band_3']]
     b.download(scene_id, bands)
-    input_path = os.path.join(path_download, scene_id[0])
 
-    send_post_request(job['job_id'], 2)
+    input_path = os.path.join(path_download, scene_id)
+
+    delete_me = []
+    rename_me = []
+    # Resize each band
+    for band in bands:
+        # file_name = '{}/B{}-geo.TIF'.format(direc_scene, b)
+        file_name = '{}/{}_B{band}.TIF'.format(input_path, scene_id, band)
+        delete_me.append(file_name)
+        file_name2 = '{}.re'.format(file_name)
+        rename_me.append(file_name2)
+        subprocess.call(['gdal_translate', '-outsize', '10%', '10%',
+                         file_name, file_name2])
+        if not os.path.exists(file_name2):
+            out = u'https://raw.githubusercontent.com/recombinators/little-worker/master/failimages/badmagicnumber.png'
+            # return out
+            # raise Exception('Bad magic number')
+    print 'done resizing 3 images'
+
+    for i, o in zip(rename_me, delete_me):
+        os.remove(i)
+        os.rename(i, o)
+
     c = Process(input_path, bands=bands, dst_path=path_download, verbose=True)
     c.run(pansharpen=False)
 
@@ -142,36 +164,27 @@ def process(job):
 
     for i in bands:
         band_output = '{}{}'.format(band_output, i)
-    file_name = '{}_bands_{}.TIF'.format(scene_id[0], band_output)
+    file_name = '{}_bands_{}.TIF'.format(scene_id, band_output)
     file_location = os.path.join(input_path, file_name)
-
-    # zip file, maintain location
-    print 'Zipping file'
-    send_post_request(job['job_id'], 3)
-    file_name_zip = '{}_bands_{}.zip'.format(scene_id[0], band_output)
-    path_to_zip = os.path.join(input_path, file_name_zip)
-    with zipfile.ZipFile(path_to_zip, 'w', zipfile.ZIP_DEFLATED) as myzip:
-        myzip.write(file_location)
 
     # upload to s3
     print 'Uploading to S3'
-    send_post_request(job['job_id'], 4)
-
-    file_location = os.path.join(input_path, file_name_zip)
+    file_location = os.path.join(input_path, file_name)
     conne = boto.connect_s3(aws_access_key_id=AWS_ACCESS_KEY_ID,
                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     b = conne.get_bucket('landsatproject')
     k = Key(b)
-    k.key = file_name_zip
+    k.key = 'pre_{}'.format(file_name)
     k.set_contents_from_filename(file_location)
     k.get_contents_to_filename(file_location)
-    hello = b.get_key(file_name_zip)
+    hello = b.get_key('pre_{}'.format(file_name))
     # make public
     hello.set_canned_acl('public-read')
 
     out = hello.generate_url(0, query_auth=False, force_http=True)
     print out
-    send_post_request(job['job_id'], 5, out)
+
+
 
     # delete files
     try:
@@ -181,10 +194,6 @@ def process(job):
         print 'error deleting files'
 
     return True
-
-
-    # generates url that works for 1 hour
-    # plans_url = plans_key.generate_url(3600, query_auth=True, force_http=True)
 
 if __name__ == '__main__':
     main()
