4bd8ae7c88dbae010de24f635f85d250094ca22b
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-05-20 16:19:12 -0700

    #32 line spacing

--- test_sqs.py
+++ test_sqs.py
@@ -3,7 +3,6 @@ import models
 import sqs
 import os
 from sqlalchemy import create_engine
-import unittest
 from moto import mock_sqs
 
 # Define AWS credentials
@@ -50,10 +49,13 @@ def conns():
     return sqs.make_SQS_connection(REGION,
                                    AWS_ACCESS_KEY_ID,
                                    AWS_SECRET_ACCESS_KEY)
+
+
 @mock_sqs
 @pytest.fixture(scope='class')
 def queue(conns):
     return sqs.get_queue(conns, COMPOSITE_QUEUE)
+
 
 @pytest.fixture(scope='class')
 def job_message():
@@ -95,38 +97,47 @@ def job_message():
 # fake_job_for_queue = [Fake_Job_Class("job", message)]
 # bad_fake_job = [Fake_Job_Class("job", ['test'])]
 
+
 def test_make_SQS_connection(conns):
     """Tests make_SQS_connection method."""
     assert COMPOSITE_QUEUE in [i.name for i in conns.get_all_queues()]
     assert PREVIEW_QUEUE in [i.name for i in conns.get_all_queues()]
+
 
 def test_get_queue(conns):
     """Tests get_queue method."""
     assert COMPOSITE_QUEUE == sqs.get_queue(conns, COMPOSITE_QUEUE).name
     assert PREVIEW_QUEUE == sqs.get_queue(conns, PREVIEW_QUEUE).name
 
+
 def test_build_result_message_message(job_message):
     assert job_message['body'] == job_message['message']['body']
+
 
 def test_build_result_message_attributes_jobid(job_message):
     assert 'Number' == job_message['message']['attributes']['job_id']['data_type']
     assert job_message['jobid'] == job_message['message']['attributes']['job_id']['string_value']
 
+
 def test_build_result_message_attributes_sceneid(job_message):
     assert 'String' == job_message['message']['attributes']['scene_id']['data_type']
     assert job_message['scene'] == job_message['message']['attributes']['scene_id']['string_value']
+
 
 def test_build_result_message_attributes_band1(job_message):
     assert 'Number' == job_message['message']['attributes']['band_1']['data_type']
     assert job_message['band1'] == job_message['message']['attributes']['band_1']['string_value']
 
+
 def test_build_result_message_attributes_band2(job_message):
     assert 'Number' == job_message['message']['attributes']['band_2']['data_type']
     assert job_message['band2'] == job_message['message']['attributes']['band_2']['string_value']
 
+
 def test_build_result_message_attributes_band3(job_message):
     assert 'Number' == job_message['message']['attributes']['band_3']['data_type']
     assert job_message['band3'] == job_message['message']['attributes']['band_3']['string_value']
+
 
 def test_send_message(conns, queue, job_message):
     sqs.send_message(conns,
@@ -135,37 +146,45 @@ def test_send_message(conns, queue, job_message):
                      job_message['message']['attributes'])
     assert queue.count() == 1
 
+
 def test_queue_size(queue):
     assert sqs.queue_size(queue) == 1
+
 
 def test_get_message(queue):
     message = sqs.get_message(queue, visibility_timeout=0)
     assert len(message) == 1
+
 
 def test_get_attributes_jobid(job_message, queue):
     message = sqs.get_message(queue, visibility_timeout=0)
     attrs = sqs.get_attributes(message)
     assert job_message['jobid'] == int(attrs['job_id'])
 
+
 def test_get_attributes_sceneid(job_message, queue):
     message = sqs.get_message(queue, visibility_timeout=0)
     attrs = sqs.get_attributes(message)
     assert job_message['scene'] == attrs['scene_id']
+
 
 def test_get_attributes_band1(job_message, queue):
     message = sqs.get_message(queue, visibility_timeout=0)
     attrs = sqs.get_attributes(message)
     assert job_message['band1'] == int(attrs['band_1'])
 
+
 def test_get_attributes_band2(job_message, queue):
     message = sqs.get_message(queue, visibility_timeout=0)
     attrs = sqs.get_attributes(message)
     assert job_message['band2'] == int(attrs['band_2'])
 
+
 def test_get_attributes_band3(job_message, queue):
     message = sqs.get_message(queue, visibility_timeout=0)
     attrs = sqs.get_attributes(message)
     assert job_message['band3'] == int(attrs['band_3'])
+
 
 def test_delete_message_from_handle(conns, queue):
     message = sqs.get_message(queue, visibility_timeout=0)
